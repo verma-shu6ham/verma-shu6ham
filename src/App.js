37d62eb6380b6ba6aa19a90151b8e84b1040d912
verma-shu6ham <verma.shu6ham@gmail.com>
2024-10-14 15:47:35 +0530

    download as pdf feature added - v1.1.0

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import './App.css';
-import React from 'react';
+import React, { useRef } from 'react';
+import html2pdf from 'html2pdf.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
 
 function App() {
+  const contentRef = useRef();
 
+  const handleDownload = () => {
+    const element = contentRef.current;
+    html2pdf()
+      .from(element)
+      .set({
+        filename: process.env.REACT_APP_PDF_NAME,
+        html2canvas: {
+          scale: 2,
+          scrollY: 0,
+        },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        pagebreak: { mode: ['css', 'legacy'] }
+      })
+      .save();
+  };
 
   return (
     <div className="App">
-      <Header />
-      <div className='mainBody'>
-        <Body />
+      <button onClick={handleDownload} className="download-button">
+        Download as PDF
+      </button>
+      <div ref={contentRef}>
+        <Header />
+        <div className='mainBody'>
+          <Body />
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
